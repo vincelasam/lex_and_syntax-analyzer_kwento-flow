c67d25e76ac8c4dcc97415857777fdd23237ac1c
James Agbon <jamesagbon09@gmail.com>
2025-12-14 20:32:48 +0800

    Changed the Token Names for Clarity

--- backend/src/types/Tokens.ts
+++ backend/src/types/Tokens.ts
@@ -15,64 +15,65 @@ export enum TokenType {
   BooleanLiteral,    // true | false
 
   // ===== Keywords =====
-  Scene,             // scene
-  Start,             // start
-  Story,             // story
-  Character,         // character
-  Transition,        // transition
-  Thru,              // thru
-  When,              // when
-  Do,                // do
-  End,               // end
-  Input,             // input
-  Rem,               // rem
-  Perceives,         // perceives
-  Masking,           // masking
-  Says,              // says
-  Log,               // log
-  Choose,            // choose
-  Default,           // default
+  K_Scene,             // scene
+  K_Start,             // start
+  K_Story,             // story
+  K_Character,         // character
+  K_Transition,        // transition
+  K_Thru,              // thru
+  K_When,              // when
+  K_Do,                // do
+  K_End,               // end
+  K_Input,             // input
+  K_Rem,               // rem
+  K_Perceives,         // perceives
+  K_Masking,           // masking
+  K_Says,              // says
+  K_Log,               // log
+  K_Choose,            // choose
+  K_Default,           // default
 
-  // ===== Reserved Data Types =====
-  Text,              // text
-  Boolean,           // boolean
-  Number,            // number
+  // ===== Reserved Words =====
+  R_Text,              // text
+  R_Boolean,           // boolean
+  R_Number,            // number
 
   // ===== Noise Words =====
-  To,                // to
-  Is,                // is
-  Then,              // then
+  N_To,                // to
+  N_Is,                // is
+  N_Then,              // then
 
   // ===== Operators =====
-  Plus,              // +
-  Minus,             // -
-  Asterisk,          // *
-  Slash,             // /
-  Modulo,            // %
-  Caret,             // ^
-  EqualTo,           // ==
-  NotEqual,          // !=
-  Less,              // <
-  LessEqual,         // <=
-  Greater,           // >
-  GreaterEqual,      // >=
-  LogicalAnd,        // &&
-  LogicalOr,         // ||
-  LogicalNot,        // !
+  OP_Plus,              // +
+  OP_Minus,             // -
+  OP_Asterisk,          // *
+  OP_Slash,             // /
+  OP_Modulo,            // %
+  OP_Caret,             // ^
+  OP_Assign,            // =
+  OP_EqualTo,           // ==
+  OP_NotEqual,          // !=
+  OP_Less_Than,              // <
+  OP_Less_Equal,         // <=
+  OP_Greater_Than,           // >
+  OP_Greater_Equal,      // >=
+  OP_Logical_And,        // &&
+  OP_Logical_Or,         // ||
+  OP_Logical_Not,        // !
 
   // ===== Delimiters =====
-  LParen,            // (
-  RParen,            // )
-  LBrace,            // {
-  RBrace,            // }
-  LBracket,          // [
-  RBracket,          // ]
-  Colon,             // :
-  Semicolon,         // ;
-  Comma,             // ,
-  Dot,               // .
-  DoubleQuote,       // "
-  SingleQuote,       // '
+  D_LParen,            // (
+  D_RParen,            // )
+  D_LBrace,            // {
+  D_RBrace,            // }
+  D_LBracket,          // [
+  D_RBracket,          // ]
+  D_Colon,             // :
+  D_Semicolon,         // ;
+  D_Comma,             // ,
+  D_Dot,               // .
+  D_DoubleQuote,       // "
+  D_SingleQuote,       // '
 
   // ===== Comments =====
   SingleLineComment, // ~~ comment until newline
@@ -86,27 +87,27 @@ export enum TokenType {
 }
 
 export const KEYWORDS: Record<string, TokenType> = {
-  "scene": TokenType.Scene,
-  "story": TokenType.Story,
-  "start": TokenType.Start,
-  "character": TokenType.Character,
-  "transition": TokenType.Transition,
-  "thru": TokenType.Thru,
-  "when": TokenType.When,
-  "do": TokenType.Do,
-  "end": TokenType.End,
-  "input": TokenType.Input,
-  "says": TokenType.Says,
-  "log": TokenType.Log,
-  "rem": TokenType.Rem,
-  "perceives": TokenType.Perceives,
-  "masking": TokenType.Masking,
-  "choose": TokenType.Choose,
-  "default": TokenType.Default,
-  "text": TokenType.Text,
-  "boolean": TokenType.Boolean,
-  "number": TokenType.Number,
-  "to": TokenType.To,
-  "is": TokenType.Is,
-  "then": TokenType.Then,
+  "scene": TokenType.K_Scene,
+  "story": TokenType.K_Story,
+  "start": TokenType.K_Start,
+  "character": TokenType.K_Character,
+  "transition": TokenType.K_Transition,
+  "thru": TokenType.K_Thru,
+  "when": TokenType.K_When,
+  "do": TokenType.K_Do,
+  "end": TokenType.K_End,
+  "input": TokenType.K_Input,
+  "says": TokenType.K_Says,
+  "log": TokenType.K_Log,
+  "rem": TokenType.K_Rem,
+  "perceives": TokenType.K_Perceives,
+  "masking": TokenType.K_Masking,
+  "choose": TokenType.K_Choose,
+  "default": TokenType.K_Default,
+  "text": TokenType.R_Text,
+  "boolean": TokenType.R_Boolean,
+  "number": TokenType.R_Number,
+  "to": TokenType.N_To,
+  "is": TokenType.N_Is,
+  "then": TokenType.N_Then,
 };
